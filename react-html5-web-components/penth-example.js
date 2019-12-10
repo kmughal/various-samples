ca2121116d577b23457a123b530e18d0ff2537b0
Khurram <khurram@Khurrams-MBP.home>
2019-12-10 19:11:29 +0000

    test penthouse

--- react-html5-web-components/penth-example.js
+++ react-html5-web-components/penth-example.js
@@ -1,18 +1,27 @@
-
-
 const stream = require("stream");
 
-function *gen() {
-    yield 'hello';
-    yield 'bye';
+function* gen() {
+  yield "hello";
+  yield "bye";
 }
-const readAsString= stream.Readable.from(gen(),{encoding: "utf8"})
+const readAsString = stream.Readable.from(gen(), { encoding: "utf8" });
 
 async function readableToString(readable) {
-    let result = "";
-    for await(const chunk of readable) {
-        result += chunk;
-    }
-    return result;
+  let result = "";
+  for await (const chunk of readable) {
+    result += chunk;
+  }
+  return result;
 }
-assert(await readableToString(readAsString),"\n line\n another line")
+// assert(await readableToString(readAsString), "\n line\n another line");
+
+const penthouse = require("penthouse");
+const fs = require("fs");
+
+penthouse({
+  url: "https://google.com",
+  cssString: "body { color: red }"
+}).then(criticalCss => {
+  // use the critical css
+  fs.writeFileSync("outfile.css", criticalCss);
+});
