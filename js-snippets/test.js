491dc0994ff110fe131c9b261de3bde8b4f345b7
Khurram <khurram@Khurrams-MacBook-Pro.local>
2019-12-11 11:43:46 +0000

    removed bad code from test

--- js-snippets/test.js
+++ js-snippets/test.js
@@ -7,16 +7,3 @@ console.groupEnd("Person")
 var p = { name : "khurram" , age: 39};
 
 console.dir(p,{ colors: true, depth: null});
-
-
-{/* <video id=v width="320" height="240" controls>
-  <source src="movie.mp4" type="video/mp4">
-  <source src="movie.ogg" type="video/ogg">
-  Your browser does not support the video tag.
-</video> */}
-
-// Just imagine that you have the following markup in your html
-
-navigator.mediaDevices.getUserMedia({video:true})
-.then(stream => document.getElementById("v").srcObject = stream)
-.catch(e => console.log(e));// this will ask user permission for camera once granted then you will be able to see video!
