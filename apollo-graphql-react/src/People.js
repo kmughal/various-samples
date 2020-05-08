5c9f82399ae80fce5ae03770f78d226c546f73da
Khurram <khurramshahzad@tfl.gov.uk>
2020-05-08 13:13:19 +0100

    use client to read data  from cache

--- apollo-graphql-react/src/People.js
+++ apollo-graphql-react/src/People.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useQuery } from "@apollo/react-hooks"
 import { FETCH_PEOPLE_QUERY } from "./fetch-actions"
 import PersonEntryForm from "./PeopleEntryForm"
-import {FIND_ALL_RECORDS_FROM_LOCAL_STORE} from "./fetch-actions"
+import { FIND_ALL_RECORDS_FROM_LOCAL_STORE } from "./fetch-actions"
 const PleaseWait = () => <p>Loading....</p>
 
 const PeopleRecords = ({ people }) => {
@@ -17,7 +17,7 @@ const PeopleRecords = ({ people }) => {
       </thead>
 
       <tbody>
-        {people.map((p,i) => {
+        {people.map((p, i) => {
           return (
             <tr key={i}>
               <td className="w-1/4 px-4 py-2">{p.name}</td>
@@ -30,21 +30,32 @@ const PeopleRecords = ({ people }) => {
   )
 }
 
-const FetchFailed = () => <p className="bg-red-600 bg-opacity-75 text-6xl text-color-white">Fetch failed</p>
+const FetchFailed = () => (
+  <p className="bg-red-600 bg-opacity-75 text-6xl text-color-white">
+    Fetch failed
+  </p>
+)
 
 const People = () => {
-  const { loading, error, data,setData } = useQuery(FIND_ALL_RECORDS_FROM_LOCAL_STORE , { pollInterval: 500})
- 
+  const {
+    loading,
+    error,
+    data,
+    client,
+  } = useQuery(FIND_ALL_RECORDS_FROM_LOCAL_STORE, { pollInterval: 1500 })
+  window.client = client
   const addPersonCompleted = () => {
-    console.log("added")
+    const dataFromCache = client.readQuery({query : FIND_ALL_RECORDS_FROM_LOCAL_STORE})
+    console.log(dataFromCache)
   }
   if (error) return <FetchFailed />
   if (loading) return <PleaseWait />
-  return ( <>
-    <PersonEntryForm addPersonCompleted={addPersonCompleted}/>
-    <PeopleRecords people={data.people} />
-    </>)
- 
+  return (
+    <>
+      <PersonEntryForm addPersonCompleted={addPersonCompleted} />
+      <PeopleRecords people={data.people} />
+    </>
+  )
 }
 
 export default People
