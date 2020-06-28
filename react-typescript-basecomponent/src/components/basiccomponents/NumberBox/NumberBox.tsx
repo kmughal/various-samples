5213df703e070dcfffce70d88351c01a667ee380
Khurram <khurramshahzad@tfl.gov.uk>
2020-06-28 13:32:57 +0100

    fix ts warning for checkbox

--- react-typescript-basecomponent/src/components/basiccomponents/NumberBox/NumberBox.tsx
+++ react-typescript-basecomponent/src/components/basiccomponents/NumberBox/NumberBox.tsx
@@ -16,7 +16,7 @@ const NumberBox: React.FC<{ numberBoxProps: NumberBoxProps }> = ({
         type="number"
         name={numberBoxProps.name}
         id={numberBoxProps.id}
-        ref={numberBoxProps.eleRef}
+        ref={numberBoxProps.eleRef as React.MutableRefObject<HTMLInputElement>}
         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
       />
       {!numberBoxProps.valid && (
